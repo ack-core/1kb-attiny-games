66611e5562a0b5a9da91e7a840a2bdc532ae5191
ka <kas.shaman@outlook.com>
2018-03-04 13:13:10 +0300

    [#] new game "beat'em up"

---
+++ beatem.cpp
@@ -0,0 +1,607 @@
+//
+// 1KB Fighting game for Attiny13A (1024 bytes flash / 64 bytes ram) with oled display 128x64 (ssd1306)
+// Author: Alexey Kaspin
+//
+// compilation flags: -mno-interrupts -DNDEBUG -Os -ffunction-sections -fdata-sections 
+//                    -fpack-struct -fshort-enums -mmcu=attiny13a -std=c++11 -fno-caller-saves -mtiny-stack -ffreestanding"  
+//
+// link flags: -nostartfiles --gc-sections -mmcu=attiny13a -mrelax
+//
+
+// default attiny13a fuses = 1MHz
+#define F_CPU 1000000L
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+// 4-wire connection to oled1306 display
+//
+#define D_DIN PORTB3 // D1
+#define D_CLK PORTB4 // D0
+#define D_DC  PORTB2
+#define D_RES PORTB1
+
+#define OLED1306_PORT_HI(p) (PORTB |= (1 << (p)))
+#define OLED1306_PORT_LO(p) (PORTB &= ~(1 << (p)))
+
+// magic
+//
+template <uint8_t... Chars> struct chars {
+private:
+    template <void (*f)(uint8_t), typename = void> inline static void _apply() {}
+    template <void (*f)(uint8_t), uint8_t Ch, uint8_t... Chs> inline static void _apply() {
+        f(Ch);
+        _apply<f, Chs...>();
+    }
+        
+public:
+    template <void (*f)(uint8_t)> static void apply() {
+        _apply<f, Chars...>();
+    }
+        
+    template <void (*f)(uint8_t)> static void __attribute__ ((noinline, used)) apply_noinline() {
+        _apply<f, Chars...>();
+    }
+};
+
+using fighter_stay = chars<
+    0b10000000,
+    0b01101100,
+    0b00011110,
+    0b00100100,
+    0b11001000
+>;
+
+using fighter_walk0 = chars<
+    0b00000000,
+    0b11001100,
+    0b00011110,
+    0b11100100,
+    0b00001000
+>;
+
+using fighter_walk1 = chars<
+    0b00000000,
+    0b00011100,
+    0b11101110,
+    0b00000100,
+    0b00001000
+>;
+
+using fighter_punch = chars<
+    0b10000000,
+    0b01100000,
+    0b00011100,
+    0b00101110,
+    0b11000100,
+    0b00000100,
+    0b00000100
+>;
+
+using fighter_kick = chars<
+    0b00010000,
+    0b00001110,
+    0b11111100,
+    0b00011100,
+    0b00000100,
+    0b00000010,
+    0b00000010
+>;
+
+using fighter_hitted = chars<
+    0b10011100,
+    0b01001110,
+    0b00011100,
+    0b11101000,
+    0b00000000
+>;
+
+using enemy_stay = chars<
+    0b11000000,
+    0b00101010,
+    0b00011100,
+    0b11101100,
+    0b00000000
+>;
+
+using enemy_punch = chars<
+    0b00000100,
+    0b11000100,
+    0b00100110,
+    0b00011100,
+    0b01101000,
+    0b10000000
+>;
+
+using enemy_hitted = chars<
+    0b11000000,
+    0b00100000,
+    0b00011100,
+    0b01101110,
+    0b10000100,
+    0b00001000
+>;
+
+using building0 = chars<
+    0b11111111,
+    0b11010101,
+    0b11111111,
+    0b11010101,
+    0b11111111,
+    0b10000000
+>;
+
+using building1 = chars<
+    0b11111100,
+    0b01010100,
+    0b01111100,
+    0b01010100,
+    0b11111100,
+    0b10000000
+>;
+
+using building2 = chars<
+    0b00110000,
+    0b11001000,
+    0b00100000,
+    0b10000000
+>;
+
+using startText = chars<
+    0b10010010,
+    0b11111111,
+    0b00001010,
+    0b00000000,
+    0b11111110,
+    0b01001011,
+    0b01001010,
+    0b11111110
+>;
+
+const uint8_t imgdat[] = {
+    0b00100000, // enemy fall animation
+    0b00010000,
+    0b11110100,
+    0b00111000,
+    0b00011100,
+    
+    0b00100000,
+    0b00100000,
+    0b01000000,
+    0b01100000,
+    0b01110000,
+    
+    0b10000000,
+    0b10000000,
+    0b11000000,
+    0b11000000,
+    0b10000000,
+    
+    0b10000000, // fighter fall animation
+    0b01110000,
+    0b00111000,
+    0b11011000,
+    0b00001000,
+    
+    0b10000000,
+    0b11100000,
+    0b01110000,
+    0b11110000,
+    0b00010000,
+        
+    0b10000000,
+    0b11000000,
+    0b11000000,
+    0b01000000,
+    0b10000000,
+    
+    0x1F, 0x11, 0x1F, // '0'  48
+    0x12, 0x1F, 0x10, // '1'  49
+    0x1D, 0x15, 0x17, // '2'  50
+    0x11, 0x15, 0x1F, // '3'  51
+    0x07, 0x04, 0x1F, // '4'  52
+    0x17, 0x15, 0x1D, // '5'  53
+    0x1F, 0x15, 0x1D, // '6'  54
+    0x01, 0x1D, 0x03, // '7'  55
+    0x1F, 0x15, 0x1F, // '8'  56
+    0x17, 0x15, 0x1F, // '9'  57
+};
+
+union compreg {
+    const uint8_t *ptr;
+    
+    struct {
+        uint8_t lo, hi;
+    };
+};
+
+// does not count as assembler using :)
+register compreg anim_ptr asm ("r2");
+register uint8_t anim_offset asm ("r4");
+register uint8_t bg_draw_offset asm ("r6");
+register uint8_t range_arg_0 asm ("r16");
+register uint8_t range_arg_1 asm ("r17");
+
+static constexpr uint8_t DISPLAY_MAX_X_COORD = 127;
+static constexpr uint8_t BACKGROUND_A_VERTICAL_OFFSET = 2;
+static constexpr uint8_t BACKGROUND_B_VERTICAL_OFFSET = 5;
+static constexpr uint8_t START_TEXT_HORIZONTAL_OFFSET = 58;
+static constexpr uint8_t SCORE_HORIZONTAL_OFFSET = 120;
+static constexpr uint8_t PLAYER_VERTICAL_OFFSET = 4;
+static constexpr uint8_t PLAYER_HORIZONTAL_OFFSET = 43;
+static constexpr uint8_t ENEMY_FIGHT_OFFSET = 50;
+
+void send_value(uint8_t value) {
+    OLED1306_PORT_LO(D_CLK);
+    
+    for (uint8_t i = 0; i < 8; i++) {
+        OLED1306_PORT_LO(D_DIN);
+        
+        if (value & 0b10000000) {
+            OLED1306_PORT_HI(D_DIN);
+        }
+        
+        value <<= 1;
+        
+        OLED1306_PORT_HI(D_CLK);
+        OLED1306_PORT_LO(D_CLK);
+    }
+}
+
+void __attribute__ ((noinline)) send_command(uint8_t cmd) {
+    OLED1306_PORT_LO(D_DC);
+    send_value(cmd);
+}
+
+void __attribute__ ((noinline)) send_data(uint8_t dat) {
+    OLED1306_PORT_HI(D_DC);
+    send_value(dat);
+}
+
+static void init_display() {
+    OLED1306_PORT_HI(D_CLK);
+    OLED1306_PORT_LO(D_RES);
+    
+    uint8_t i = 255;
+    do {
+        asm volatile("nop");
+    }
+    while(--i);
+    
+    OLED1306_PORT_HI(D_RES);
+    
+    chars<0xAE, 0xD5, 0x80, 0xA8, 0x3f, 0x20, 0x00, 0xAF>::apply<send_command>();
+}
+
+void set_coord_range_r_imp() {
+    send_command(0x21);
+    send_command(range_arg_0);
+    send_command(range_arg_1); 
+}
+
+void set_page_range_r_imp() {
+    send_command(0x22);
+    send_command(range_arg_0);
+    send_command(range_arg_1);
+}
+
+#define set_coord_range_r(s, e) do {\
+    range_arg_0 = s; \
+    range_arg_1 = e; \
+    set_coord_range_r_imp(); } while(0);
+
+#define set_page_range_r(s, e) do {\
+    range_arg_0 = s; \
+    range_arg_1 = e; \
+    set_page_range_r_imp(); } while(0);
+
+void delay(uint8_t ms_x10) {
+    do {
+        _delay_ms(10);
+    }
+    while (--ms_x10);
+}
+
+void drawFighter(uint8_t counter) {
+    uint8_t frame = counter & 0b110;
+            
+    if (frame == 0) {
+        fighter_stay::apply<send_data>();
+    }
+    else if (frame == 2) {
+        fighter_walk0::apply_noinline<send_data>();
+    }
+    else if (frame == 4) {
+        fighter_walk1::apply<send_data>();
+    }
+    else {
+        fighter_walk0::apply_noinline<send_data>();
+    }
+}
+
+void send_data_c(uint8_t dat) {
+    if (++range_arg_0 > bg_draw_offset) {
+        OLED1306_PORT_HI(D_DC);
+        send_value(dat);
+    }
+    else {
+        uint8_t i = 16;
+        do {
+            asm volatile("nop"); // also
+        }
+        while(--i);
+    }
+}
+
+void fill_ground() {
+    do {
+        send_data_c(0b10000000);
+    }
+    while(--range_arg_1);
+}
+
+void fall() {
+    anim_ptr.ptr = imgdat + range_arg_0;
+    range_arg_0 = 3;
+                            
+    do {
+        range_arg_1 = 5;
+        
+        do {
+            send_data(*anim_ptr.ptr);
+            anim_ptr.lo++;
+        }
+        while(--range_arg_1);
+        delay(20);
+    }
+    while(--range_arg_0);
+    delay(40);    
+}
+
+void draw_score() {
+    range_arg_0 = 3;
+    anim_ptr.ptr = imgdat + 30 + anim_offset;
+        
+    do {
+        send_data(*anim_ptr.ptr);
+        anim_ptr.lo++;
+    }
+    while(--range_arg_0);
+
+    send_data(0);
+}
+
+// Compiler will generate code that fill memory (22 bytes)
+void main() __attribute__((naked, section(".init9")));
+void main() {
+    DDRB  = 0b00011110;
+    PORTB = 0b00000001;
+
+    init_display();
+    game_start:
+    
+    set_page_range_r(0, 7);
+    range_arg_1 = DISPLAY_MAX_X_COORD;
+    set_coord_range_r_imp();    
+
+    { // clear display 64x128 (1-bit per pixel)
+        uint16_t i = 8 * 128;
+    
+        do {
+            send_data(0x00);
+        }
+        while (--i);    
+    }    
+
+    range_arg_0 = BACKGROUND_A_VERTICAL_OFFSET;
+    set_page_range_r_imp();
+    range_arg_0 = START_TEXT_HORIZONTAL_OFFSET;
+    set_coord_range_r_imp();
+    
+    // chinese verb "beat"
+    startText::apply<send_data>();
+    
+    // wait for button press
+    while((PINB & 0x1) == 0);
+            
+    int8_t life = 10;
+    uint8_t rnd;
+    
+    union {
+        uint16_t word;
+        struct {
+            uint8_t lo;
+            uint8_t hi;
+        };
+    } score;
+    
+    score.word = 0;
+    
+    // cycle per enemy
+    while (true) {        
+        uint8_t enemy_offset = 100;
+        uint8_t enemy_punch_counter = 0;
+        uint8_t player_punch_counter = 0;
+        uint8_t player_walk_counter = 0;
+        uint8_t prev_key = 0;
+                
+        // draw cycle
+        while (true) {       
+            set_page_range_r(0, 0);
+            range_arg_1 = DISPLAY_MAX_X_COORD;
+            set_coord_range_r_imp(); 
+
+            // top-left life bar
+            {
+                int8_t i = 0;
+                do {                    
+                    send_data(i >= life ? 0x19 : 0x1F);
+                }
+                while(++i < 10);
+            }
+
+            range_arg_0 = SCORE_HORIZONTAL_OFFSET;
+            set_coord_range_r_imp();
+
+            // top-right score
+            anim_offset = score.hi;
+            draw_score();
+            anim_offset = score.lo;
+            draw_score();
+
+            set_page_range_r(BACKGROUND_A_VERTICAL_OFFSET, BACKGROUND_A_VERTICAL_OFFSET); 
+            set_coord_range_r(0, DISPLAY_MAX_X_COORD);
+            
+            range_arg_0 = 0;
+            
+            // background image
+            {
+                uint8_t i = 2;
+                
+                do {
+                    building2::apply_noinline<send_data_c>();
+                    building0::apply_noinline<send_data_c>();
+                    building0::apply_noinline<send_data_c>();
+                    building1::apply_noinline<send_data_c>();
+                    range_arg_1 = 19;
+                    fill_ground();
+                    building2::apply_noinline<send_data_c>();
+                    building0::apply_noinline<send_data_c>();
+                    building2::apply_noinline<send_data_c>();
+                    building2::apply_noinline<send_data_c>();
+                    range_arg_1 = 8;
+                    fill_ground();
+                    building1::apply_noinline<send_data_c>();
+                    building2::apply_noinline<send_data_c>();
+                    range_arg_1 = 21;
+                    fill_ground();
+                    building0::apply_noinline<send_data_c>();
+                    building1::apply_noinline<send_data_c>();
+                    range_arg_1 = 18;
+                    fill_ground();
+                }
+                while(--i);
+            }      
+            
+            set_page_range_r(BACKGROUND_B_VERTICAL_OFFSET, BACKGROUND_B_VERTICAL_OFFSET);     
+
+            range_arg_1 = range_arg_0 = 128;            
+            fill_ground();
+              
+            set_page_range_r(PLAYER_VERTICAL_OFFSET, PLAYER_VERTICAL_OFFSET);            
+            set_coord_range_r(PLAYER_HORIZONTAL_OFFSET, PLAYER_HORIZONTAL_OFFSET + 14);
+            
+            // clear place for player and enemy
+            {
+                uint8_t i = 15;
+                do {
+                    send_data(0);
+                }
+                while (--i);
+            }
+            
+            if (enemy_offset > ENEMY_FIGHT_OFFSET) {
+                ++player_walk_counter;
+                
+                if (player_walk_counter & 0b1) {
+                    --enemy_offset;
+    
+                    if ((player_walk_counter & 0b110) == 0b110) {
+                        bg_draw_offset = (++bg_draw_offset) & 0x7f;
+                    }
+                }
+            }
+            else {
+                if (enemy_punch_counter == 0) {
+                    uint8_t key = PINB & 0x1;
+                    uint8_t diff = key - prev_key;
+                    prev_key = key;
+
+                    // button pressed
+                    if (diff == 1) {
+                        if (player_punch_counter == 0) {
+                            player_punch_counter = 6;
+                        }
+                        else {
+                            fighter_kick::apply<send_data>();
+                            set_coord_range_r(ENEMY_FIGHT_OFFSET, ENEMY_FIGHT_OFFSET + 4);
+                            
+                            // enemy fall
+                            range_arg_0 = 0;
+                            fall();
+                            
+                            score.lo += 3;
+                            if (score.lo == 30 ) {
+                                score.lo = 0;
+                                score.hi += 3;
+
+                                if (score.hi == 30 ) {
+                                    score.lo = 0;
+                                    score.hi = 0;
+                                }
+                            }                          
+                            
+                            break;
+                        }
+                    }
+                    else { // enemy can punch us
+                        if (rnd < 20) {
+                            enemy_punch_counter = 6;
+                            life--;
+                        }
+                        
+                        uint8_t h = rnd & 0x80;
+                        rnd <<= 1;
+                        if (h == 0) {
+                            rnd ^= 43;
+                        }
+                    }
+                }
+            }
+                        
+            if (enemy_punch_counter) {
+                enemy_punch_counter--;
+                fighter_hitted::apply<send_data>();
+            }
+            else if (player_punch_counter > 3) {
+                fighter_punch::apply<send_data>();
+            }
+            else {
+                drawFighter(player_walk_counter);
+            }
+            
+            range_arg_1 = DISPLAY_MAX_X_COORD;
+            
+            if (enemy_punch_counter > 2) {
+                range_arg_0 = ENEMY_FIGHT_OFFSET - 1;
+                set_coord_range_r_imp();
+                enemy_punch::apply<send_data>();
+            }
+            else {
+                range_arg_0 = enemy_offset;
+                set_coord_range_r_imp();
+                
+                if (player_punch_counter) {
+                    player_punch_counter--;
+                    enemy_hitted::apply<send_data>();
+                }
+                else {
+                    enemy_stay::apply<send_data>();
+                    
+                    // player fall
+                    if (life <= 0) {
+                        set_coord_range_r(PLAYER_HORIZONTAL_OFFSET, PLAYER_HORIZONTAL_OFFSET + 4);
+                        
+                        range_arg_0 = 15;
+                        fall();
+                        
+                        goto game_start;
+                    }
+                }
+            }
+            
+            delay(1);
+        }
+    }
+}
+
--- racing.cpp
+++ racing.cpp
@@ -1,9 +1,124 @@
+//
+// 1KB Racing game for Attiny13A (1024 bytes flash / 64 bytes ram) with oled display 128x64 (ssd1306)
+// Author: Alexey Kaspin
+//
+// compilation flags: -mno-interrupts -DNDEBUG -Os -ffunction-sections -fdata-sections
+//                    -fpack-struct -fshort-enums -mmcu=attiny13a -std=c++11 -fno-caller-saves -mtiny-stack -ffreestanding"
+//
+// link flags: -nostartfiles --gc-sections -mmcu=attiny13a -mrelax
+//
+
+// default attiny13a fuses = 1MHz
 #define F_CPU 1000000L
 
 #include <avr/io.h>
 #include <util/delay.h>
 
-#include "oled1306_tiny.h"
+// 4-wire connection to oled1306 display
+//
+#define D_DIN PORTB3 // D1
+#define D_CLK PORTB4 // D0
+#define D_DC  PORTB2
+#define D_RES PORTB1
+
+#define OLED1306_PORT_HI(p) (PORTB |= (1 << (p)))
+#define OLED1306_PORT_LO(p) (PORTB &= ~(1 << (p)))
+
+template <uint8_t... Chars> struct chars {
+private:
+    template <void (*f)(uint8_t), typename = void> inline static void _apply() {}
+    template <void (*f)(uint8_t), uint8_t Ch, uint8_t... Chs> inline static void _apply() {
+        f(Ch);
+        _apply<f, Chs...>();
+    }
+
+    template <void (*f)(uint8_t), typename = void> inline static void _apply_inv() {}
+    template <void (*f)(uint8_t), uint8_t Ch, uint8_t... Chs> inline static void _apply_inv() {
+        _apply_inv<f, Chs...>();
+        f(Ch);
+    }
+        
+public:
+    template <void (*f)(uint8_t)> static void apply() {
+        _apply<f, Chars...>();
+    }
+        
+    template <void (*f)(uint8_t)> static void apply_inv() {
+        _apply_inv<f, Chars...>();
+    }
+};
+
+static void send_value(uint8_t value) {
+    OLED1306_PORT_LO(D_CLK);
+            
+    for (uint8_t i = 0; i < 8; i++) {
+        OLED1306_PORT_LO(D_DIN);
+                
+        if (value & 0b10000000) {
+            OLED1306_PORT_HI(D_DIN);
+        }
+                
+        value <<= 1;
+                
+        OLED1306_PORT_HI(D_CLK);
+        OLED1306_PORT_LO(D_CLK);
+    }
+}
+    
+static void __attribute__ ((noinline)) send_command(uint8_t cmd) {
+    OLED1306_PORT_LO(D_DC);
+    send_value(cmd);
+}
+
+static void __attribute__ ((noinline)) send_data(uint8_t dat) {
+    OLED1306_PORT_HI(D_DC);
+    send_value(dat);
+}
+
+static void set_page(uint8_t page) {
+    send_command(0x22);
+    send_command(page);
+    send_command(7);
+}
+
+static void set_coord_range(uint8_t start, uint8_t end) {
+    send_command(0x21);
+    send_command(start);
+    send_command(end);
+}
+
+static void set_coord(uint8_t coord) {
+    send_command(0x21);
+    send_command(coord);
+    send_command(127);
+}
+
+static void clear() {
+    set_page(0);
+    set_coord(0);
+    
+    uint16_t i = 8 * 128;
+    
+    do {
+        send_data(0x00);
+    }
+    while (--i);
+}
+
+static void init() {
+    OLED1306_PORT_HI(D_CLK);
+    OLED1306_PORT_LO(D_RES);
+    
+    volatile uint8_t i = 255;
+    do {
+        asm volatile("nop");
+    }
+    while(--i);
+    
+    OLED1306_PORT_HI(D_RES);
+    
+    chars<0xAE, 0xD5, 0x80, 0xA8, 0x3f, 0x20, 0x00, 0xAF>::apply<send_command>();
+}
 
 static const uint8_t CAR_CENTER = 58;
 static const uint8_t CAR_MAX_OFFSET = 12;
@@ -17,7 +132,7 @@ static const uint8_t BARRIER_STEP_2 = 96;
 static const uint8_t BARRIER_STEP_4 = 176;
 static const uint8_t START_FRAME_TIME = 30;
 
-using car_side = oled1306::chars<
+using car_side = chars<
     0b00000000,
     0b01110000,
     0b11101100,
@@ -32,7 +147,7 @@ using car_side = oled1306::chars<
     0b00000000
 >;
 
-using car_center = oled1306::chars<
+using car_center = chars<
     0b00000000,
     0b01111000,
     0b11101110,
@@ -47,7 +162,7 @@ using car_center = oled1306::chars<
     0b00000000
 >;
 
-using car_over = oled1306::chars<
+using car_over = chars<
     0b00010100,
     0b11011001,
     0b11011110,
@@ -83,7 +198,7 @@ static uint8_t numbers[20] = {
     0x5C,
     0x74,
     0x78,
-    0x64,   
+    0x64,
     0x64,
     0x1C,
     0x6C,
@@ -114,9 +229,9 @@ static uint8_t score_txt[17] = {
 
 void draw_score_num(uint8_t num) {
     uint8_t *img = (uint8_t *)numbers + (num << 1);
-    oled1306::send_data(*img++);
-    oled1306::send_data(*img);
-    oled1306::send_data(0x0);
+    send_data(*img++);
+    send_data(*img);
+    send_data(0x0);
 }
 
 void delay(uint16_t ms) {
@@ -126,38 +241,44 @@ void delay(uint16_t ms) {
     while(--ms);
 }
 
-int main(void) {    
+// Compiler will generate code that fill memory (22 bytes)
+void main() __attribute__((naked, section(".init9")));
+void main() {
+    
     DDRB  = 0b00011110;
     PORTB = 0b00000001;
     
-    oled1306::init();
-        
+    init();
+    
     while (true) {
-        oled1306::clear();    
-        oled1306::set_page(3);
-        oled1306::set_coord(58);    
-        oled1306::draw_text4x(txt("Go?"));      
-              
-        while ((PINB & 0x1) == 0);        
-        oled1306::clear();
+        clear();
+        
+        set_page(3);
+        set_coord(58);
+        
+        chars<0x0E, 0x11, 0x1D, 0x00, 0x0C, 0x12, 0x0C, 0x00, 0x01, 0x15, 0x03>::apply<send_data>();
+        
+        while ((PINB & 0x1) == 0);
+
+        clear();
 
         { // horizon
             uint8_t fill = 128;
             while(--fill) {
-                oled1306::send_data(0x80);
+                send_data(0x80);
             }
         }
-    
-        oled1306::set_coord(40);
+        
+        set_coord(40);
         for (uint8_t i = 5; i; i--) {
-            oled1306::set_page(i);
-            oled1306::chars<0b00000011, 0b00001100, 0b00110000, 0b11000000>::apply_inv<oled1306::send_data>();
+            set_page(i);
+            chars<0b00000011, 0b00001100, 0b00110000, 0b11000000>::apply_inv<send_data>();
         }
 
-        oled1306::set_coord(68);
+        set_coord(68);
         for (uint8_t i = 1; i < 6; i++) {
-            oled1306::set_page(i);
-            oled1306::chars<0b00000011, 0b00001100, 0b00110000, 0b11000000>::apply<oled1306::send_data>();
+            set_page(i);
+            chars<0b00000011, 0b00001100, 0b00110000, 0b11000000>::apply<send_data>();
         }
         
         uint8_t car_current_offset = CAR_START_OFFSET;
@@ -180,17 +301,17 @@ int main(void) {
         while (true) {
             delay(frame_time);
             
-            oled1306::set_coord_range(64, 64);
-            oled1306::set_page(2);
+            set_coord_range(64, 64);
+            set_page(2);
             
             { // center road line
                 uint8_t t0 = *(uint8_t *)&roadline[roadline_index];
                 uint8_t t1 = *((uint8_t *)&roadline[roadline_index] + 1);
 
-                oled1306::send_data(t0);
-                oled1306::send_data(t1);
-                oled1306::send_data(t1);
-                oled1306::send_data(t1);
+                send_data(t0);
+                send_data(t1);
+                send_data(t1);
+                send_data(t1);
 
                 if (++roadline_index >= 4) {
                     roadline_index = 0;
@@ -207,45 +328,45 @@ int main(void) {
                 }
                 else {
                     car_current_offset += car_inc;
-                }                                
+                }
             }
 
-            oled1306::set_page(5);
-            oled1306::set_coord(car_current_offset);
+            set_page(5);
+            set_coord(car_current_offset);
 
             if (car_current_offset < CAR_LEFT_SIDE) {
                 if ((barrier_side & 0x1) && barrier_index > BARRIER_DANGER_INDEX) {
-                    car_over::apply<oled1306::send_data>();
+                    car_over::apply<send_data>();
                     break;
                 }
                 else {
-                    car_side::apply<oled1306::send_data>();
+                    car_side::apply<send_data>();
                 }
             }
             else if (car_current_offset > CAR_RIGHT_SIDE) {
                 if ((barrier_side & 0x1) == 0 && barrier_index > BARRIER_DANGER_INDEX) {
-                    car_over::apply<oled1306::send_data>();
+                    car_over::apply<send_data>();
                     break;
                 }
                 else {
-                    car_side::apply_inv<oled1306::send_data>();
+                    car_side::apply_inv<send_data>();
                 }
             }
             else {
-                car_center::apply<oled1306::send_data>();
+                car_center::apply<send_data>();
             }
-        
+            
             { // barriers
-                oled1306::set_page(barrier_page);
-                oled1306::set_coord(barrier_x);
+                set_page(barrier_page);
+                set_coord(barrier_x);
                 
                 {
                     uint8_t draw_count = barrier_index >> 4;
                     while (--draw_count) {
-                        oled1306::send_data(0x0);
+                        send_data(0x0);
                     }
                 }
-            
+                
                 if (barrier_index < BARRIER_STEP_1) {
                     barrier_index += 1;
                 }
@@ -257,11 +378,11 @@ int main(void) {
                 }
                 else {
                     barrier_index = BARRIER_MIN_INDEX;
-                    barrier_side = (barrier_side << 1) | (barrier_side >> 7);  
+                    barrier_side = (barrier_side << 1) | (barrier_side >> 7);
                     
                     if (frame_time > 1) {
                         frame_time--;
-                    }                        
+                    }
                     
                     if (++score0 >= 10) {
                         score0 = 0;
@@ -275,35 +396,34 @@ int main(void) {
                 uint8_t offset = barrier_index >> 2;
                 uint8_t draw_count = offset >> 2;
                 uint8_t page = (draw_count >> 1);
-                    
+                
                 barrier_x = 64 + ((barrier_side & 0x1) ? (-draw_count - page) : (1 + page));
                 
-                oled1306::set_page(page);
-                oled1306::set_coord(barrier_x);
+                set_page(page);
+                set_coord(barrier_x);
                 barrier_page = page;
 
                 while (--draw_count) {
-                    oled1306::send_data(0b00000001 << (offset & 7));
+                    send_data(0b00000001 << (offset & 7));
                 }
             }
-        
-            oled1306::set_page(7);
-            oled1306::set_coord(51);
+            
+            set_page(7);
+            set_coord(51);
 
             {
                 uint8_t i = 16;
                 do {
-                    oled1306::send_data(score_txt[i]);                
+                    send_data(score_txt[i]);
                 }
                 while (i--);
             }
 
             draw_score_num(score2);
             draw_score_num(score1);
-            draw_score_num(score0);        
-        }        
+            draw_score_num(score0);
+        }
         
         delay(2500);
     }
 }
-
